2714a5580352a4c858b2399c80690cddae2ee585
Pong <65143370@g.cmru.ac.th>
2026-06-21 23:02:41 +0700

    แก้

--- admin/admin.php
+++ admin/admin.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== tru
     exit;
 }
 
-include 'db.php';
+include '../db.php';
 
 
 if (isset($_GET['action']) && $_GET['action'] === 'logout') {
